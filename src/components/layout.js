c313ecf8c4ee1d858c1a4b9d29152bc83fe4ca5f
hemeshvpatel <hemesh.v.patel@gmail.com>
2020-12-07 22:10:02 -0600

    updated build

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Header from "./Header"
+import Header from "../components/Header"
 import { GlobalStyle } from "./styles/GlobalStyles"
 import Footer from "../components/Footer"
 
